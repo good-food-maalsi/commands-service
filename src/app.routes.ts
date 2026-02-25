1acd0d0e563b4b8ef423f0779f47682bed4e87bb
Célian HAMON <hamoncelian@gmail.com>
2026-02-25 13:24:28 +0100

    fix

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -3,8 +3,6 @@ import { Elysia } from "elysia";
 import { OrderController } from "./Order/order.controller.js";
 import { prismaPlugin } from "./Plugin/prisma.js";
 
-const routes = new Elysia({ prefix: "/commands" })
-    .use(prismaPlugin)
-    .use(OrderController);
+const routes = new Elysia().use(prismaPlugin).use(OrderController);
 
 export { routes as AppRoutes };
